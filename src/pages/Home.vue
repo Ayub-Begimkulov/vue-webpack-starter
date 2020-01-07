1f4df327a187bad37056903d0c9ec97f8e7cb786
Ayub Begimkulov <begimkulov963@gmail.com>
2020-01-07 18:17:51 +0300

    changed images

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
     <router-link to="/about">About</router-link>
-    <img src="../assets/img/arrow.svg" alt />
-    <img src="../assets/img/tree.jpeg" alt />
-    <img src="../assets/img/ayub.jpeg" alt />
+    <p>This is an svg icon and should be loaded as utf-8 encoded.</p>
+    <img src="../assets/img/arrow.svg" />
+    <p>This is a png img and it's less 10kb so it should be loaded as base64.</p>
+    <img src="../assets/img/tree.png" />
+    <p>This is an 8k jpg image and should be loaded as a file.</p>
+    <img src="../assets/img/greenbridge.jpg" />
   </div>
 </template>
 
 <style lang="scss" scoped>
 div {
-  a {
-    color: red;
-  }
+  margin-left: 10px;
+
   img {
     display: block;
     width: 100px;
